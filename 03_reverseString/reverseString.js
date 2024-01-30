998ac9c46cb9000dcd0b27198af516a947a3f768
Observer001 <46171900+Observer001@users.noreply.github.com>
2024-01-30 18:53:17 +0530

    Made all the required changes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let index = string.length-1
+  let result = ''
+  while(index > -1) {
+    result += string.charAt(index)
+    index--
+  }
 
+  return result
 };
 
 // Do not edit below this line
