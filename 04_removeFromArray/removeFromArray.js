13216ca4922d5fd312583aefebb89097128e5000
Observer001 <46171900+Observer001@users.noreply.github.com>
2024-01-30 18:50:28 +0530

    Made required changes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(args) {
+  let array = arguments[0]
+  let argLen = arguments.length
+  for(let i=1; i < argLen; i++) {
+    let val = arguments[i]
+    if(array.indexOf(val) !== -1)
+      array.splice(array.indexOf(val), 1)
+  }
 
+  return array
 };
 
 // Do not edit below this line
