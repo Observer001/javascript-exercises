01303115045bf04bde469391658d7f206ee3f86e
Observer001 <46171900+Observer001@users.noreply.github.com>
2024-02-14 21:43:17 +0530

    Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current)
+  // console.log(arr);
+  // if(arr.length === 0)
+  //   return 0;
 
+  // let sol=1;
+  // arr.forEach(val => sol *= val);
+  // return sol;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num <= 1)
+    return 1;
+
+  let sol = 1;
+  for(let i=1; i<=num; i++)
+    sol = sol*i;
+
+  return sol;
 };
 
 // Do not edit below this line
